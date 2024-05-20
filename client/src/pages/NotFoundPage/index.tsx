29f7b82b1196d3e858ffa39fbf8e5734f2441cfc
OxeoH <egor33429@gmail.com>
2024-05-21 00:55:05 +0300

    add notfound page

--- client/src/pages/NotFoundPage/index.tsx
+++ client/src/pages/NotFoundPage/index.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { MAIN_ROUTE } from "../../routes/utils/consts";
+import { Stack, Typography } from "@mui/material";
+import { SentimentVeryDissatisfied } from "@mui/icons-material";
 
 export default function NotFoundPage() {
-  return <h1>Oops..Page not found</h1>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate(MAIN_ROUTE);
+    }, 3000);
+
+    // Очистка таймера при размонтировании компонента
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <Stack
+      width="100%"
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Typography variant="h2" color="InfoText" sx={{ my: 10 }}>
+        Упс..Страница не найдена
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        color={(theme) => theme.palette.primary.main}
+        sx={{ my: 10 }}
+      >
+        Вы будете перенаправлены через 3 секунды...
+      </Typography>
+      <SentimentVeryDissatisfied fontSize="large" color="primary" />
+    </Stack>
+  );
 }
