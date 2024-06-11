d8fc8ff41c9f0d597f339db15203f52b09dc29da
OxeoH <egor33429@gmail.com>
2024-06-11 20:05:56 +0300

    add settings page widgets

--- client/src/pages/WelcomePage/index.tsx
+++ client/src/pages/WelcomePage/index.tsx
@@ -1,5 +1,21 @@
+import { Stack, Typography } from "@mui/material";
 import React from "react";
 
 export default function index() {
-  return <h1>Что нового?</h1>;
+  return (
+    <Stack
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      width="100%"
+      textAlign="center"
+    >
+      <Typography variant="h2" color="primary" my={20}>
+        Облачное хранилище для кафедральных документов
+      </Typography>
+      <Typography variant="h6" color="text">
+        Перед использованием требуется пройти регистрацию
+      </Typography>
+    </Stack>
+  );
 }
